6c83560318911d20684c6a19652bc4b9a923071a
alexmushi <111137234+alexmushi@users.noreply.github.com>
2022-12-01 23:47:10 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,46 @@
-#include "luchador.h"
-#include "enemigo.h"
+/*
+ * Proyecto Juego
+ * A01705550
+ * 2/12/2022
+ */
 #include<iostream>
+
 using namespace std;
+
+#include "alquimista.h"
+#include "caballero.h"
+#include "curandero.h"
+#include "enemigo.h"
 
 int main(){
 
+	Alquimista pepepo("Pepe pociones");
 	Caballero parzival("Parzival");
 	Curandero zenyatta("Zenyatta");
-	Alquimista pepe("pepe pociones");
+	Enemigo enemi(100, 10, "enemigo");
 
-	Enemigo enem(50, 10)
+	parzival.atacar(enemi);
+	enemi.atacar(parzival);
+	pepepo.atacar(enemi);
+	enemi.atacar(pepepo);
+	enemi.atacar(pepepo);
+	enemi.atacar(pepepo);
+	enemi.atacar(zenyatta);
+	enemi.atacar(zenyatta);
+	enemi.atacar(zenyatta);
+	zenyatta.curarMax();
+	parzival.crit(enemi);
+	pepepo.pociones();
+	pepepo.curar();
 
-	cout << parzival.getNombre() << endl;
-	cout << zenyatta.getNombre() << endl;
-	cout << pepe.getNombre() << endl;
+	cout << parzival.getNom() << endl;
+	cout << parzival.getHp() << endl;
+	cout << pepepo.getNom() << endl;
+	cout << pepepo.getHp() << endl;
+	cout << zenyatta.getNom() << endl;
+	cout << zenyatta.getHp() << endl;
+	cout << enemi.getNom() << endl;
+	cout << enemi.getHp();
 
-	enem.atk(parzival)
-	parzival.crit(enem)
-	enem.atk(zenyatta)
-	zenyatta.curarMax()
-	pepe.masPociones()
 
 }
